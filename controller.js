a0fbfc36d4d3f54325c64a7d016d8a8d4c259c35
Adam Cox <adamcox.dev@gmail.com>
2022-09-09 14:07:45 -0600

    added files

---
+++ controller.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcryptjs');
+const chats = [];
+
+module.exports = {
+    createMessage: (req, res) => {
+        console.log(req.body);
+        const {pin, message} = req.body;
+
+        for(let i =0; i < chats.length; i++){
+            const existingPin = bcrypt.compareSync(pin, chats[i].pinHash);
+            if(existingPin){
+                chats[i].messages.push(message);
+
+                let existingSecureMsg = {...chats[i]}
+                delete existingSecureMsg.pinHash
+
+                return res.status(200).send(existingSecureMsg);
+            }
+        }
+
+    const salt = bcrypt.genSaltSync(5);
+    const pinHash = bcrypt.hashSync(pin, salt);
+
+    // console.log(pin)
+    // console.log(salt)
+    // console.log(pinHash)
+
+    let msgObj = {
+        pinHash,
+        messages: [message]
+    }
+
+    chats.push(msgObj);
+
+    let secureMessage = {...msgObj};
+    delete secureMessage.pinHash;
+
+
+    res.status(200).send(secureMessage);
+
+
+}
+}
